3aaea45cc010d071f4dc6416ca39084bc98ccb99
OussamaGhamian <oussamaalghamian@gmail.com>
2019-12-16 13:52:57 +0200

    step4 is done

---
+++ Exercises/Javascript-Basics-01/step_4.js
@@ -0,0 +1,6 @@
+function show() {
+  var name = document.getElementById("name").value;
+  var surname = document.getElementById("surname").value;
+  var city = document.getElementById("city").value;
+  alert(`Nom : ${name}\nPrenome : ${surname}\nVille : ${city}`);
+}
