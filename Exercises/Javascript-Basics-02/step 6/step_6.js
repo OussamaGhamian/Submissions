f8ce24ddb824522445288381e3809bdc9f7ea45a
OussamaGhamian <oussamaalghamian@gmail.com>
2019-12-17 05:38:23 +0200

    step6_js2 is done

---
+++ Exercises/Javascript-Basics-02/step 6/step_6.js
@@ -0,0 +1,19 @@
+var arr = document.querySelectorAll("img");
+arr.forEach(function(item) {
+  item.addEventListener("mouseover", function() {
+    var i;
+    var j = 1;
+    for (i = 0; i < document.querySelectorAll("img").length; i++) {
+      document.querySelectorAll("img")[i].src = "images/image" + j++ + "_2.jpg";
+    }
+  });
+});
+arr.forEach(function(item) {
+  item.addEventListener("mouseleave", function() {
+    var i;
+    var j = 1;
+    for (i = 0; i < document.querySelectorAll("img").length; i++) {
+      document.querySelectorAll("img")[i].src = "images/image" + j++ + ".jpg";
+    }
+  });
+});
