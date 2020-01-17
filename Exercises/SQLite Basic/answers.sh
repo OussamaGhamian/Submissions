563dbbe2022b235f155d60d0f0a3b3ed7ef64f7d
OussamaGhamian <oussamaalghamian@gmail.com>
2020-01-17 11:09:01 +0200

    basicQueries

---
+++ Exercises/SQLite Basic/answers.sh
@@ -0,0 +1,22 @@
+problem 0: 
+select * from students
+problem 1:
+select name from students
+problem 2:
+select * from students where age > 30
+problem 3:
+select name from students where age = 30 and gender = 'F'
+problem 4:
+select points from students where name like 'Alex' 
+problem 5:
+insert into students values(8,"Oussama" , 22 , 'M' , 999)
+problem 6:
+update students
+set points = 306
+where name like 'Basma'
+problem 7:
+update students
+set points = points - 10
+where name like 'Alex'
+
+
