651e8369e6ca9bc8a4148f49892306347a95c4e3
OussamaGhamian <oussamaalghamian@gmail.com>
2020-01-22 12:09:54 +0200

    step4

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -14,6 +14,21 @@ app.get("/test", (req, res) => {
 app.get("/time", (req, res) => {
   res.send({ status: 200, message: `${current}` });
 });
+app.get("/hello/:id?", (req, res) => {
+  if (req.params.id)
+    res.send({ status: 200, message: `Hello,${req.params.id}` });
+  else res.send({ starus: 200, message: "Hello" });
+});
+app.get("/search=:se?", (req, res) => {
+  if (req.params.se)
+    res.send({ status: 200, message: "ok", data: `${req.params.se}` });
+  else
+    res.send({
+      status: 500,
+      error: true,
+      message: "you have to provide a search"
+    });
+});
 app.listen(port, () => {
   console.log(`server is running at: http://localhost:${port}/`);
 });
