aef641175546b182137973c874f8769d9d11a3d3
OussamaGhamian <oussamaalghamian@gmail.com>
2020-01-22 11:19:41 +0200

    step3

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -1,8 +1,18 @@
+let time = new Date();
+let current = time.getHours() + " : " + time.getMinutes();
+//
 const express = require("express");
 const app = express();
-const port = "3033";
+const port = "3034";
+//routes
 app.get("/", (req, res) => {
   res.send("Hello World from Oussama");
+});
+app.get("/test", (req, res) => {
+  res.send({ status: 200, message: "ok" });
+});
+app.get("/time", (req, res) => {
+  res.send({ status: 200, message: `${current}` });
 });
 app.listen(port, () => {
   console.log(`server is running at: http://localhost:${port}/`);
